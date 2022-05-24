2e397a7de25b524afc98a40cdc6791ead2fa341b
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-05-24 16:55:15 +0530

    Time: 119 ms (84.98%), Space: 61.3 MB (61.78%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -4,7 +4,7 @@ public:
         int i;
         
         //sorting
-        sort(nums.begin(),nums.end());
+       /* sort(nums.begin(),nums.end());
         for(i=1;i<nums.size();i++){
             if(nums[i] == nums[i-1])
                 return nums[i];
@@ -12,12 +12,34 @@ public:
         return nums[i];
         
         //xor
+        //not on more than 2 duplicates
+        int x = 0, y = 0;
+        for(i=0;i<nums.size();i++){
+            x = x xor nums[i];
+            y = y xor i;
+        }
+        return abs(x-y);
+        */
+        //floyds cycle but here
+        int slow = nums[0], fast = nums[0];
         
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow; 
         
         //add
+        //not for more than 1 duplicate
         int add = 0;
         for(i=0;i<nums.size();i++) add += nums[i];
-        int sum = nums.size() * (nums.size()+1) / 2;
-        return (add-sum);
+        int sum = (nums.size() * (nums.size()-1)) / 2;
+        return (add - sum);
     }
 };

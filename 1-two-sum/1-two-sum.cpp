d18316577fa73784a060cd46f1b1381e1cacd66f
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-06-01 00:54:47 +0530

    Time: 25 ms (47.40%), Space: 12.5 MB (5.49%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         multimap<int,int> mp;
-        int i;
-        for(i=0;i<nums.size();i++){
-            mp.insert(pair<int, int>(nums[i],i));
-        }
-        auto it1 = mp.begin();
-
+		
+        for(int i=0; i<nums.size(); i++)
+            mp.insert(pair<int, int>(nums[i], i));
+       
+	    auto it1 = mp.begin();
         for(; it1 != mp.end(); it1++){
-            if (mp.find(target - it1->first) != mp.end() && mp.find(target - it1->first) != it1) break;
+	       //check if (target - current element) is present
+            if (mp.find(target - it1->first) != mp.end() && 
+			    //make sure it is not the current element itself
+			    mp.find(target - it1->first) != it1)
+				break;
         }
         return {it1->second, mp.find(target - it1->first)->second};
     }

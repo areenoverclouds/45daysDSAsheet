4299c755cd08caf2e6dfadf2c1811f318cf98ce9
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-06-05 23:38:09 +0530

    Time: 1279 ms (14.29%), Space: 139.5 MB (42.86%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -4,15 +4,19 @@ public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& opr) {
         map<int, int> mp;
         int i;
-        for(i=0;i<nums.size();i++){
+		//insert element and index in map
+        for(i=0; i<nums.size(); i++){
             mp.insert(make_pair(nums[i], i));
         }
-        for(i=0;i<opr.size();i++){
+		
+        for(i=0; i<opr.size(); i++){
             auto pos = mp.find(opr[i][0]);
-            int put = opr[i][1], to = pos->second;
-            nums[to] = put;
+            int put = opr[i][1], at = pos->second;
+            nums[at] = put;
+			//erase the current occurence in map
             mp.erase(pos->first);
-            mp.insert(make_pair(put, to));
+			//add new pair in map
+            mp.insert(make_pair(put, at));
         }
         return nums;
     }

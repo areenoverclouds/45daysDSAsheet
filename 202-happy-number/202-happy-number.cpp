80166f533c5e5a812a25637f1ce4f1e476fe5266
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-07-04 23:13:56 +0530

    Time: 3 ms (61.39%), Space: 5.7 MB (93.97%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int x = n;
+        int sum = 0;
+        while(x){
+            int h = x%10;
+            sum += h*h;
+            x = x/10;
+        }
+        if(sum == 1 ||  sum == 7)
+            return true;
+        else if(sum < 10)
+            return false;
+        else
+            return isHappy(sum);
+    }
+};

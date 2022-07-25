553d084b3be7d2949a546456d2681ee2735349ca
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-07-25 21:13:53 +0530

    Time: 65 ms (9.91%), Space: 23.5 MB (5.96%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-int dp[301];
-int breakit(set<string> s, int i, string target){
+
+int breakit(set<string> s, int i, string target, vector<int> &dp){
     if(i >= target.size()) return 1;
     if(dp[i] != -1) return dp[i];
     string temp = "";
     for(int j = i; j < target.size(); j++){
         temp += target[j];
         if(s.find(temp) != s.end()){
-            if(breakit(s, j+1, target)) return dp[i] = 1;
+            if(breakit(s, j+1, target, dp)) return dp[i] = 1;
         }
     }
     return dp[i] = 0;
 }
 bool wordBreak(string target, vector<string> arr) {
     set<string> s;
-    memset(dp, -1, sizeof(dp));
+    vector<int> dp(301, -1);
     for(auto it : arr) s.insert(it);
-    return breakit(s, 0, target);
+    return breakit(s, 0, target, dp);
 }
 };

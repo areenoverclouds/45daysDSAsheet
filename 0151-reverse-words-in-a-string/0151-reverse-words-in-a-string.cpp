543d9f4e6564e9a28825a1c831e4460f3f29f72c
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-11-13 14:18:09 +0530

    Time: 11 ms (62.21%), Space: 8.8 MB (38.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -12,7 +12,6 @@ public:
             else if(ch != ' ') temp += ch;
         }
         words.push_back(temp);
-        while(words[words.size()-1] == " ") words.pop_back();
         reverse(words.begin(), words.end());
         temp = "";
         for(auto w: words) temp += w;
